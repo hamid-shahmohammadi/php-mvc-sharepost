83ef90773dc6e81feb4c5511e039bd908172143e
Hamid Shah Mohammadi <hamid.shahmohamadi@gmail.com>
2019-06-03 09:12:01 +0330

    login

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
   define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
   define('DB_PASS', '');
-  define('DB_NAME', '_YOUR_DBNAME_');
+  define('DB_NAME', 'phpmvc');
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
